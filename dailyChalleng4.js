4bd44ee131ba12f993d893c5240f9a08787b5370
EstherOkiri <est.okiri@gmail.com>
2024-02-24 00:26:12 -0800

    modified daily challenge 4

--- dailyChalleng4.js
+++ dailyChalleng4.js
@@ -2,4 +2,17 @@ function groupBy(carArray,PropertyName){
     let groupedArray = {};
     carArray.forEach()
         let propertyValue = car[PropertyName];
+        
+        return groupedArray;
     }
+
+const carArray = [
+    {name: 'Mercedes', year: 2012},
+    {name: 'Xtrail', year: 2010},
+    {name: 'bmw', year: 2014},
+    {name: 'Subaru', year: 2010},
+    {name: 'Toyota Rav4', year: 2014}
+];
+ let groupedByYear = groupBy(carArray,'year');
+ console.log(groupedByYear);
+
